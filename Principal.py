7aef88016a13e554120ee57ca598749656da1f98
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-06 14:53:09 -0300

    Apenas executa as ações das bibliotecas.

---
+++ Principal.py
@@ -0,0 +1,5 @@
+from ValidaRequisicao import ListaEquipamentosG5
+
+site = input("Qual é o nome do site?\nObs: O mesmo deve ser descrito exatamente igual como está no G5.\nSite: ")
+
+ListaEquipamentosG5().run(site)
