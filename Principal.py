fd9d255ccf527bfbe4eceac5f6265fd422cdc12a
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-07 00:43:54 -0300

    Inserido campo para usuário preencher com dados de acesso ao g5config

--- Principal.py
+++ Principal.py
@@ -1,4 +1,16 @@
 from ValidaRequisicao import ListaEquipamentosG5
+from WebScrapingG5 import ListaSitesG5
+
+print("Favor preencha os campos solicitados.")
+email = input("Informe seu email: ")
+senha = input("Informe sua senha: ")
+ListaSitesG5().login(email, senha)
+
+while(ListaSitesG5().testaLogin() == False):
+    print("Favor preencha os campos solicitados.")
+    email = input("Informe seu email: ")
+    senha = input("Informe sua senha: ")
+    ListaSitesG5().login(email, senha)
 
 site = input("Qual é o nome do site?\nObs: O mesmo deve ser descrito exatamente igual como está no G5.\nSite: ")
 
