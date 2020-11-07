b574205d4b360f47b33e388f463c987f83a85ade
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-07 00:43:12 -0300

    Corrigido listarEquipamentos e inserido função testaLogin

--- WebScrapingG5.py
+++ WebScrapingG5.py
@@ -16,12 +16,18 @@ class ListaSitesG5:
     def __init__(self):
         pass
 
-    def login(self):
+    def login(self, email, senha):
         driver.get("https://g5config.oxyn.com.br/app/sites_list")
-        #Os dados devem ser de uma pessoa que contém acesso ao G5 config.
-        driver.find_element_by_id('user_email').send_keys('')
-        driver.find_element_by_id('user_senha').send_keys('')
+        #Datas must be from someone that has g5config access
+        driver.find_element_by_id('user_email').send_keys('{}'.format(email))
+        driver.find_element_by_id('user_senha').send_keys('{}'.format(senha))
         driver.find_element_by_xpath("//input[@value='Entrar']").click()
+
+    def testaLogin(self):
+        if driver.find_elements_by_xpath("//p[contains(text(), 'Usuário ou senha inválido')]"):
+            return False
+        elif driver.find_elements_by_xpath("//thead//th[@align='center']"):
+            return True
 
     def selectSite(self, site):
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), '{}')]".format(site))))
@@ -32,7 +38,6 @@ class ListaSitesG5:
         print("Validando sites do G5.")
         time.sleep(1)
         print("Aguarde, isso pode levar alguns minutos...")
-        self.login()
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//table[@id='newspaper-b']//tbody")))
         sites = driver.find_elements_by_xpath("//tbody//tr//td")
         for i in sites:
@@ -45,10 +50,12 @@ class ListaSitesG5:
 
     def listarEquipamentoG5(self, site):
         self.selectSite(site)
+        #Warrant that I'm into equipment page
         WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//th[@id='equipamentos']")))
+        #Wait until detect equipments text
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//table[@id='newspaper-b_sub']//tbody//tr[1]//td[2]//a")))
         equipamentos = driver.find_elements_by_xpath("//table[@id='newspaper-b_sub']//tbody//tr//td[2]//a")
         for i in equipamentos:
+            #Include equipment name into list listaEquipamentos
             listaEquipamentos.append(i.text)
             print(i.text)
-        time.sleep(2)
-        print("Fim da busca.")
