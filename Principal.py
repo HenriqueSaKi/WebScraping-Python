084e8cc396f19030793a782e81b94d974e8130b9
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-09 09:25:03 -0300

    Inserido loop para nova pesquisa
    - Bug no loop. É solicitado para o usuário realizar o login duas vezes.

--- Principal.py
+++ Principal.py
@@ -1,18 +1,25 @@
 from ValidaRequisicao import ListaEquipamentosG5
 from WebScrapingG5site import ListaSitesG5
+import getpass
 
-print("Favor preencha os campos solicitados.")
-email = input("Informe seu email: ")
-senha = input( "Informe sua senha: " )
-ListaSitesG5().login(email, senha)
 
-while(ListaSitesG5().testaLogin() == False):
-    print("Usuário ou senha inválido!")
+def preencherLogin():
     print("Favor preencha os campos solicitados.")
     email = input("Informe seu email: ")
-    senha = input("Informe sua senha: ")
+    senha = getpass.getpass("Informe sua senha: ")
     ListaSitesG5().login(email, senha)
 
-site = input("Qual é o nome do site?\nObs: O mesmo deve ser descrito exatamente igual como está no G5.\nSite: ")
+def preencherSite():
+    site = input("Qual é o nome do site?\nObs: O mesmo deve ser descrito exatamente igual como está no G5.\nSite: ")
+    ListaSitesG5().verificaSiteG5(site)
 
-ListaEquipamentosG5().run(site)
+answer = 's'
+while answer == 's':
+    preencherLogin()
+    while(ListaSitesG5().testaLogin() == False):
+        preencherLogin()
+
+    site = input("Qual é o nome do site?\nObs: O mesmo deve ser descrito exatamente igual como está no G5.\nSite: ")
+
+    ListaEquipamentosG5().run(site)
+    answer = input("Deseja pesquisar outro equipamento?? ")
