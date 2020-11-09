d4198dde645f7b103a840ac8e0fc7b2bc13b58de
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-08 22:43:06 -0300

    Atualizado para comunicar com o site G5

--- Principal.py
+++ Principal.py
@@ -1,12 +1,13 @@
 from ValidaRequisicao import ListaEquipamentosG5
-from WebScrapingG5 import ListaSitesG5
+from WebScrapingG5site import ListaSitesG5
 
 print("Favor preencha os campos solicitados.")
 email = input("Informe seu email: ")
-senha = input("Informe sua senha: ")
+senha = input( "Informe sua senha: " )
 ListaSitesG5().login(email, senha)
 
 while(ListaSitesG5().testaLogin() == False):
+    print("Usuário ou senha inválido!")
     print("Favor preencha os campos solicitados.")
     email = input("Informe seu email: ")
     senha = input("Informe sua senha: ")
