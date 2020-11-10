78c53af70a92b8d256dc416e859f7bcde785d0c7
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-10 17:04:14 -0300

    Split aplicado com sucesso

--- TrataNomenclaturaEquip.py
+++ TrataNomenclaturaEquip.py
@@ -5,13 +5,22 @@ class TrataLista:
         pass
 
     def removeAcentos(self, equipamento):
-        equipamento = normalize('NFKD', equipamento).encode('ASCII', 'ignore').decode('ASCII')
-        return equipamento
+        return normalize('NFKD', equipamento).encode('ASCII', 'ignore').decode('ASCII')
 
     def caixaBaixa(self, equipamento):
-        self.removeAcentos(equipamento)
-        return equipamento.lower()
+        return self.removeAcentos(equipamento).lower()
 
-#Função de split da nomenclatura do equipamento
+    def separaPalavras(self, equipamento):
+        return self.caixaBaixa(equipamento).split(' ')
+
+    def compara(self, equipamento):
+        for i in self.separaPalavras(equipamento):
+            if i == self.caixaBaixa('climatizacao'):
+                return "Deu certo!"
+            else:
+                return "Não deu certo."
+
+retorna = TrataLista().compara('Climatização - Módulo 1')
+print(retorna)
 #Função de extração de informação importante (Isolamento, CAG, TI, Centro Cirúrgico/CC, Arsenal, etc...)
 #Função run retornando a lista com os valores filtrados
