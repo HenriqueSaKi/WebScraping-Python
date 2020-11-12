c262935f718af906326f79cc2ec1a64060626d1c
Henrique-Kisaki <satoshikisaki@hotmail.com>
2020-11-12 16:21:53 -0300

    Funções atualizadas

--- ClassificaArea.py
+++ ClassificaArea.py
@@ -1,12 +1,15 @@
 from TrataNomenclaturaEquip import TrataLista as TL
+from PontuacaoInicial import Pontuacao as PI
+from PontuacaoFinal import Pontuacao as PF
+
 
 class GrauDeRisco:
     def __init__(self):
         # Define as strings que podem conter na nomenclatura do equipamento
-        self.C = ['', '', '', '', ''] #Critico
-        self.S = ['', '', '', '', ''] #Substancial
-        self.M = ['', '', '', '', ''] #Moderado
-        self.A = ['', '', '', '', ''] #Aceitável
+        self.C = ['CAG', '', '', '', '']  # Critico
+        self.S = ['', '', '', '', '']  # Substancial
+        self.M = ['', '', '', '', '']  # Moderado
+        self.A = ['', '', '', '', '']  # Aceitável
 
     def defineGrau(self, equipamento):
         # Risco à vida
@@ -25,13 +28,14 @@ class GrauDeRisco:
         elif TL().compara(equipamento, self.A) != '':
             return 1
 
+
 class Afetacao:
     def __init__(self):
-        #Define as strings que podem conter na nomenclatura do equipamento
-        self.G = ['', '', '', '', '', '', ''] #Global
-        self.L = ['', '', '', '', '', '', ''] #Local
+        # Define as strings que podem conter na nomenclatura do equipamento
+        self.G = ['CAG', '', '', '', '', '', '', '']  # Global
+        self.L = ['Isolamento', 'Cirúrgico', 'Obstétrico', 'Sala', 'CC', 'CO', 'UTI', '', '']  # Local
 
-    def defineZona(self, equipamento): #Local ou Global
+    def defineZona(self, equipamento):  # Local ou Global
         # Geral
         if TL().compara(equipamento, self.G) != '':
             return 2
@@ -40,10 +44,11 @@ class Afetacao:
         elif TL().compara(equipamento, self.L) != '':
             return 1
 
+
 class TipoComponente:
     def __init__(self):
-        self.P = ['', '', '', '', '']
-        self.U = ['', '', '', '', '']
+        self.P = ['Ventilador', 'Exaustor', 'Fancoil', 'Multimedidor', 'Hidrômetro']
+        self.U = ['Torre', 'BAGP', 'BAGS', 'BAC', 'Rack']
 
     def defineTipo(self, equipamento):
         # Parte de um sistema
@@ -54,12 +59,26 @@ class TipoComponente:
         elif TL().compara(equipamento, self.U) != '':
             return 1
 
-#Classificação sem definição do código da área
+
+# Classificação sem definição do código da área
 class ClassificacaoInicial:
+    def __init__(self, equipamento):
+        self.GDR = GrauDeRisco().defineGrau(equipamento)
+        self.AFT = Afetacao().defineZona(equipamento)
+        self.TC = TipoComponente().defineTipo(equipamento)
+
+    def pontuacaoInicial(self, grau, afetacao, tipo):
+        resultado = [grau, afetacao, tipo]
+        return resultado
+
+    def run(self):
+        return PI().inicial(self.pontuacaoInicial(self.GDR, self.AFT, self.TC))
+
+
+# Classificação após definicao do código da área
+class Classificacao:
     def __init__(self):
         pass
 
-#Classificação após definicao do código da área
-class ClassificacaoFinal:
-    def __init__(self):
-        pass
+    def run(self, equipamento):
+        return ClassificacaoInicial(equipamento).run() + PF().incremento(equipamento)
